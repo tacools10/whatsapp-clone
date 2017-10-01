c4c58f0224d41f0822bc4355ef9edcb1c3d40b02
Thomas Cools <tacools10@gmail.com>
2017-09-30 20:46:29 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {ChatsPage} from "../pages/chats/chats";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    ChatsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +19,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    ChatsPage
   ],
   providers: [
     StatusBar,
