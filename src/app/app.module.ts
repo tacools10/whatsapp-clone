dc82ac73cabbb5b488d90da17cc17b780299dece
Thomas Cools <tacools10@gmail.com>
2017-09-30 21:09:07 -0400

    Basic chat functionality running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { MomentModule } from 'angular2-moment';
 import { MyApp } from './app.component';
 import {ChatsPage} from "../pages/chats/chats";
 
@@ -14,7 +14,8 @@ import {ChatsPage} from "../pages/chats/chats";
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    MomentModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,19 +1,21 @@
 import {Component} from '@angular/core';
 import {Observable} from "rxjs";
 import * as moment from 'moment';
+import {Chat, MessageType} from "../../models";
+
 
 @Component({
   templateUrl: 'chats.html'
 })
 export class ChatsPage {
 
-  chats: Observable<any[]>;
+  chats: Observable<Chat[]>;
 
   constructor() {
     this.chats = this.findChats();
   }
 
-  private findChats(): Observable<any[]> {
+  private findChats(): Observable<Chat[]> {
     return Observable.of([
       {
         _id: '0',
@@ -21,7 +23,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
         lastMessage: {
           content: 'You on your way?',
-          createdAt: moment().subtract(1, 'hours').toDate()
+          createdAt: moment().subtract(1, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -30,7 +33,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
         lastMessage: {
           content: 'Hey, it\'s me',
-          createdAt: moment().subtract(2, 'hours').toDate()
+          createdAt: moment().subtract(2, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -39,7 +43,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
         lastMessage: {
           content: 'I should buy a boat',
-          createdAt: moment().subtract(1, 'days').toDate()
+          createdAt: moment().subtract(1, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -48,7 +53,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
         lastMessage: {
           content: 'Look at my mukluks!',
-          createdAt: moment().subtract(4, 'days').toDate()
+          createdAt: moment().subtract(4, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -57,10 +63,21 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
         lastMessage: {
           content: 'This is wicked good ice cream.',
-          createdAt: moment().subtract(2, 'weeks').toDate()
+          createdAt: moment().subtract(2, 'weeks').toDate(),
+          type: MessageType.TEXT
         }
       }
     ]);
+  }
+
+  removeChat(chat: Chat): void {
+    this.chats = this.chats.map((chatsArray: Chat[]) => {
+      const chatIndex = chatsArray.indexOf(chat);
+      if (chatIndex !== -1) {
+        chatsArray.splice(chatIndex, 1);
+      }
+      return chatsArray;
+      });
   }
 
 }
